98e05428b082284125033563e83bfb4afd8f089c
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-12 19:35:44 +0300

    Added methods encrypt and decrypt in VigenereCipher class

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,2 +1,37 @@
-package task5;public class VigenereCipher {
+package task5;
+
+public class VigenereCipher {
+    public void encrypt(String str, String key){
+        String stringToEncode = str.toUpperCase();
+        String keyword = key.toUpperCase();
+        int j = 0;
+        String encodedText = "";
+        for(int i = 0; i < stringToEncode.length(); i++){
+            int n = (stringToEncode.charAt(i) + keyword.charAt(j)) % 26;
+            n += 'a';
+            encodedText += (char)(n);
+            j++;
+            if(j >= keyword.length()){
+                j=0;
+            }
+        }
+        System.out.println(encodedText);
+    }
+    public void decrypt(String str, String key){
+        String stringToDecode = str.toUpperCase();
+        String keyword = key.toUpperCase();
+        int j = 0;
+        String decodedText = "";
+        for(int i = 0; i < stringToDecode.length(); i++){
+            int n = (stringToDecode.charAt(i) - keyword.charAt(j) + 26) % 26;
+            n += 'a';
+            decodedText += (char)(n);
+            j++;
+            if(j >= keyword.length()){
+                j=0;
+            }
+        }
+        System.out.println(decodedText);
+    }
+    
 }

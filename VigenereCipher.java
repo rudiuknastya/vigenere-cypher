c57285fa8182d70c08532efe5fc7f65324670173
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-14 16:43:41 +0300

    Added method work for user input in VigenereCipher class and method main for running program in Main class

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,7 +1,9 @@
 package task5;
 
+import java.util.Scanner;
+
 public class VigenereCipher {
-    public void encrypt(String str, String key){
+    private void encrypt(String str, String key){
         String stringToEncode = str.toUpperCase();
         String keyword = key.toUpperCase();
         int j = 0;
@@ -17,7 +19,7 @@ public class VigenereCipher {
         }
         System.out.println(encodedText);
     }
-    public void decrypt(String str, String key){
+    private void decrypt(String str, String key){
         String stringToDecode = str.toUpperCase();
         String keyword = key.toUpperCase();
         int j = 0;
@@ -33,5 +35,24 @@ public class VigenereCipher {
         }
         System.out.println(decodedText);
     }
-    
+    public void work(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like: 1.Encrypt 2.Decrypt ");
+        System.out.println("Your Enter: ");
+        String s = scanner.nextLine();
+        int choise = Integer.parseInt(s);
+        if(choise == 1){
+            System.out.println("Enter text to encrypt: ");
+            String text = scanner.nextLine();
+            System.out.print("Enter key: ");
+            String key = scanner.nextLine();
+            encrypt(text,key);
+        } else if (choise == 2) {
+            System.out.println("Enter text to decrypt: ");
+            String text = scanner.nextLine();
+            System.out.print("Enter key: ");
+            String key = scanner.nextLine();
+            decrypt(text,key);
+        }
+    }
 }
